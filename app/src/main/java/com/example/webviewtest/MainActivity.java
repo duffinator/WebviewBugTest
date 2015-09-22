949858e705e8bc859ed20a1c6999f2bddf513f1d
Steffen Wellner <steffen.wellner@tobit.com>
2015-09-22 12:05:38 +0200

    Initial Project

---
+++ app/src/main/java/com/example/webviewtest/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.webviewtest;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        WebView webView = (WebView)findViewById(R.id.webView);
+        WebView.setWebContentsDebuggingEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.contains("example.com"))
+                    return true;
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                Log.e(MainActivity.class.getSimpleName(), "onPageStarted: " + url);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                Log.e(MainActivity.class.getSimpleName(), "onPageFinished: " + url);
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+                Log.e(MainActivity.class.getSimpleName(), "onReceivedError");
+            }
+        });
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/test.html");
+    }
+}
